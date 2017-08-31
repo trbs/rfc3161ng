08431e1e4028c2bd215cad22cef8dd47604ad8a5
trbs <trbs@trbs.net>
2017-08-31 17:25:41 +0200

    add classifiers

--- setup.py
+++ setup.py
@@ -13,12 +13,25 @@ setup(
     long_description=long_description,
     author='Benjamin Dauvergne',
     author_email='bdauvergne@entrouvert.com',
+    platforms=['any'],
+    classifiers=[
+        'Environment :: Console',
+        'Intended Audience :: Developers',
+        'Intended Audience :: Information Technology',
+        'Intended Audience :: System Administrators',
+        'License :: OSI Approved :: MIT License',
+        'Operating System :: OS Independent',
+        'Programming Language :: Python :: 2.7',
+        'Programming Language :: Python :: 3.6',
+        'Topic :: Communications',
+        'Topic :: Software Development :: Libraries :: Python Modules',
+    ],
     packages=['rfc3161'],
     install_requires=[
         'pyasn1',
         'python-dateutil',
         'pyasn1_modules',
         'requests',
-        'M2Crypto',
+        'cryptography',
     ]
 )
